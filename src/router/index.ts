aa085d154f677f06c1ea0c987e1ae4114c488df5
df123 <hws184433274hws@live.com>
2025-10-22 23:15:02 +0800

    feat(router): 增强路由守卫逻辑，处理动态路由和标签页缓存

--- src/router/index.ts
+++ src/router/index.ts
@@ -98,6 +98,7 @@ const whiteList = ["/login", "/auth/callback", "/auth/silent-callback"];
 const { VITE_HIDE_HOME } = import.meta.env;
 
 router.beforeEach(async (to: ToRouteType, _from, next) => {
+  console.log("keepAlive:", to.meta?.keepAlive);
   if (to.meta?.keepAlive) {
     handleAliveRoute(to, "add");
     // 页面整体刷新和点击标签页刷新
@@ -129,12 +130,51 @@ router.beforeEach(async (to: ToRouteType, _from, next) => {
     if (VITE_HIDE_HOME === "true" && to.fullPath === "/welcome") {
       next({ path: "/error/404" });
     }
+
     if (_from?.name) {
       // name为超链接
       if (externalLink) {
         openLink(to?.name as string);
         NProgress.done();
       } else {
+        if (_from.fullPath.includes("/auth/callback")) {
+          if (
+            usePermissionStoreHook().wholeMenus.length === 0 &&
+            to.path !== "/login"
+          ) {
+            initRouter().then((router: Router) => {
+              if (!useMultiTagsStoreHook().getMultiTagsCache) {
+                const { path } = to;
+                const route = findRouteByPath(
+                  path,
+                  router.options.routes[0].children
+                );
+                getTopMenu(true);
+                // query、params模式路由传参数的标签页不在此处处理
+                if (route && route.meta?.title) {
+                  if (isAllEmpty(route.parentId) && route.meta?.backstage) {
+                    // 此处为动态顶级路由（目录）
+                    const { path, name, meta } = route.children[0];
+                    useMultiTagsStoreHook().handleTags("push", {
+                      path,
+                      name,
+                      meta
+                    });
+                  } else {
+                    const { path, name, meta } = route;
+                    useMultiTagsStoreHook().handleTags("push", {
+                      path,
+                      name,
+                      meta
+                    });
+                  }
+                }
+              }
+              // 确保动态路由完全加入路由列表并且不影响静态路由（注意：动态路由刷新时router.beforeEach可能会触发两次，第一次触发动态路由还未完全添加，第二次动态路由才完全添加到路由列表，如果需要在router.beforeEach做一些判断可以在to.name存在的条件下去判断，这样就只会触发一次）
+              if (isAllEmpty(to.name)) router.push(to.fullPath);
+            });
+          }
+        }
         toCorrectRoute();
       }
     } else {
